27a4d45a7ebfabf6d956471b06fc73ae292f83f0
Ekaterina Zarudnaya <zar.catherine@gmail.com>
2020-03-22 21:13:39 +0000

    Automatically backed up by Learn

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -138,41 +138,15 @@ describe "#display_board in 'lib/display_board.rb" do
       expect(rows[4]).to eq(" O | X | O ")
     end
 
-    it 'prints an entire board full of Xs' do
-      board = ["X", "X", "X", "X", "X", "X", "X", "X", "X"]
-
-      output = capture_puts{ display_board(board) }
-      rows = output.split("\n")
-
-      expect(rows[0]).to eq(" X | X | X ")
-      expect(rows[1]).to eq("-----------")
-      expect(rows[2]).to eq(" X | X | X ")
-      expect(rows[3]).to eq("-----------")
-      expect(rows[4]).to eq(" X | X | X ")
-    end
-
-
-   it 'prints arbitrary arrangements of the board' do
-      board = ["X", "O", "X", "O", "X", "X", "O", "X", "O"]
-
-      output = capture_puts{ display_board(board) }
-      rows = output.split("\n")
-
-      expect(rows[0]).to eq(" X | O | X ")
-      expect(rows[1]).to eq("-----------")
-      expect(rows[2]).to eq(" O | X | X ")
-      expect(rows[3]).to eq("-----------")
-      expect(rows[4]).to eq(" O | X | O ")
-    end
       # Should you want to write your own test for this situation,
       # read the following code and comments.
 
       # Can you copy the syntax of the tests above to write a test for a board
       # entirely filled with Xs?"
-
+  it 'prints a board entirely filled with Xs' do
       # Define the board with values that should create the desired output
       # *** Edit the line below ***
-      #board = [" ", " ", " ", " ", " ", " ", " ", " ", " "] # This is not correct
+      board = ["X", "X", "X", "X", "X", "X", "X", "X", "X"] # This is not correct
 
       # Don't touch the following lines.
       output = capture_puts{ display_board(board) } if defined?(display_board)
@@ -192,11 +166,11 @@ describe "#display_board in 'lib/display_board.rb" do
 
       # *** Edit the lines below ***
       # *** Uncomment the lines below ***
-      # expect(rows[0]).to eq("   |   |   ")
-      # expect(rows[1]).to eq("-----------")
-      # expect(rows[2]).to eq("   |   |   ")
-      # expect(rows[3]).to eq("-----------")
-      # expect(rows[4]).to eq("   |   |   ")
+      expect(rows[0]).to eq(" X | X | X ")
+      expect(rows[1]).to eq("-----------")
+      expect(rows[2]).to eq(" X | X | X ")
+      expect(rows[3]).to eq("-----------")
+      expect(rows[4]).to eq(" X | X | X ")
 
       # *** Comment the line below by adding a # at the line start ***
     #  expect(true).to be(true)
